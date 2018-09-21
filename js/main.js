990043d5e7a2a0b168afae47abdef6f37ced564e
gaow <gaow@ftxmall.com>
2018-09-21 18:38:57 +0800

    gw

--- js/main.js
+++ js/main.js
@@ -46,30 +46,143 @@ $(function() {
 		9-12分黄色性格
 		13-16分蓝色性格
 		17-20分绿色性格
-	 */	
-	var character = ['红色性格','黄色性格','蓝色性格','绿色性格'];
+	 */
+	var character = ['红色性格', '黄色性格', '蓝色性格', '绿色性格'];
 	//性格对应详情
-	var characterDetails = [
-		{
-			text:['阳光心态，积极快乐','激情澎湃，梦想万岁','热情开朗，喜欢交友','童心未泯，富有趣味','乐于助人，不记愁苦','善于表达，调动气氛','真诚信任，感染四方','乐在变化，创新意识']
+	var characterDetails = [{
+			text: ['阳光心态，积极快乐', '激情澎湃，梦想万岁', '热情开朗，喜欢交友', '童心未泯，富有趣味', '乐于助人，不记愁苦', '善于表达，调动气氛', '真诚信任，感染四方', '乐在变化，创新意识']
 		},
 		{
-			text:['目标导向，永无止境','求胜欲望，战胜对方','斗天斗地，敢说敢做','坚定自信，永不言败','控制情绪，抗压力强','坦率直接，实用主义','快速决断，敢冒风险','抓大放小，高效行动']
+			text: ['目标导向，永无止境', '求胜欲望，战胜对方', '斗天斗地，敢说敢做', '坚定自信，永不言败', '控制情绪，抗压力强', '坦率直接，实用主义', '快速决断，敢冒风险', '抓大放小，高效行动']
 		},
 		{
-			text:['思想深邃，独立思考','成熟稳重，安全放心','情感细腻，体贴入微','一诺千金，忠诚情谊','计划周详，注重规则','讲究精确，迷恋细节','考虑全面，善于分析','执着有恒，坚持到底']
+			text: ['思想深邃，独立思考', '成熟稳重，安全放心', '情感细腻，体贴入微', '一诺千金，忠诚情谊', '计划周详，注重规则', '讲究精确，迷恋细节', '考虑全面，善于分析', '执着有恒，坚持到底']
 		},
 		{
-			text:['中庸之道，稳定低调','乐知天命，与世无争','毕生无火，巧卸冲突','镇定自若，处事不惊','天性宽容，耐心柔和','笑遍天涯，冷面幽默','先人后己，与取先予','领导风格，以人为本']
+			text: ['中庸之道，稳定低调', '乐知天命，与世无争', '毕生无火，巧卸冲突', '镇定自若，处事不惊', '天性宽容，耐心柔和', '笑遍天涯，冷面幽默', '先人后己，与取先予', '领导风格，以人为本']
 		}
 	]
-	
-	
+
+	//第二题判断男孩还是女孩
+	$('.gender').click(function() {
+		var type = $(this).attr('data-gender');
+		if (type == 1) {
+			$('#genderShow').text('男孩');
+		} else {
+			$('#genderShow').text('女孩');
+		}
+	});
+
+	var topicNum = []; //第三题选中个数
+	$('.topic-two').click(function() {
+		var type = $(this).attr('data-type');
+		var has_bgk = $(this).hasClass('btn-bgk-active');
+		if (has_bgk) {
+			topicNum.push(type);
+			if (topicNum.length == 2) { //等于2的时候判断结果页该展示的机型
+				$.ajax({
+					url: './js/data.json',
+					type: 'get',
+					datatype: 'json',
+					success: function(data) {
+						var check, checkreverse;
+						check = topicNum[0] + topicNum[1]; //选中值
+						checkreverse = topicNum[1] + topicNum[0];
+
+						for (var key in data.computer) {
+							if (key == check) {
+								console.log(data.computer[key]);
+							} else if (key == checkreverse) {
+								console.log(data.computer[key]);
+							}
+						}
+					}
+				})
+			}
+			if (topicNum.length >= 2) { //大于等于2的时候跳转下一题
+				mySwiper.slideTo(4);
+			}
+		} else {
+			for (var i = 0; i < topicNum.length; i++) {
+				if (topicNum[i] == type) {
+					topicNum.splice(i, 1);
+				}
+			}
+		}
+	})
+
+	//性格标签查看详情页
+	$('#watchDetails').on('touchend', function() {
+		mySwiper.slideTo(12);
+	});
+	//结果页
+	$('#goBack').on('touchend', function() {
+		mySwiper.slideTo(10);
+	});
+
+
+
+	//第三题多选的处理（交叉排列组合）
+
+	//显示对应的标签
+	var labelMethods = function(data) {
+		var label = [{
+			type: 'a',
+			text: '注重外表'
+		}, {
+			type: 'b',
+			text: '喜欢旅行'
+		}, {
+			type: 'c',
+			text: '爱玩'
+		}, {
+			type: 'd',
+			text: '理智'
+		}, {
+			type: 'e',
+			text: '喜欢旅行'
+		}, {
+			type: 'f',
+			text: '怀旧'
+		}, {
+			type: 'g',
+			text: '生活品质有需求'
+		}];
+
+		var label_1 = data[0];
+		var label_2 = data[1];
+		for (var key in label) {
+			if (label[key].type == label_1) {
+				$('#label1').text(label[key].text)
+			}
+			if (label[key].type == label_2) {
+				$('#label2').text(label[key].text)
+			}
+		}
+	}
+
+
+	//显示性格对应的8点
+	var gradeMethods = function(data) {
+		var arr = [];
+		var item = data.text;
+		for (var j = 0; j < item.length; j++) {
+			arr.push(item[j])
+		}
+		var html = '';
+		for (var i = 0; i < 3; i++) { //只显示3条
+			html += '<div class="col-6" style="line-height:1.8rem;font-size:0.8rem;">' + arr[i] + '</div>';
+		}
+		$('#gradeMsg').html(html);
+	}
+
+
+
 	var mySwiper = new Swiper('.swiper-container', {
 		// direction: 'vertical', //滑动方向
-
+		// initialSlide: 9,
 		allowTouchMove: 0, //是否可以左右滑动
-		speed: 1000, //切换速度
+		speed: 100, //切换速度
 
 		effect: 'cube', //切换效果
 
@@ -86,123 +199,57 @@ $(function() {
 			slideChangeTransitionEnd: function() {
 				swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
 
-				// this.activeIndex = 3;
+
 				if (this.activeIndex == 9) {
 					//根据分数显示性格
-					if(gradeNum >= 5 && gradeNum <= 8 ){
-						$('#gradeEnd').text(character[0]).css({'background':'red'});
+					if (gradeNum >= 5 && gradeNum <= 8) {
+						$('#gradeEnd').text(character[0]).css({
+							'background': 'red'
+						});
 						gradeMethods(characterDetails[0]);
 					}
-					if(gradeNum >= 9 && gradeNum <= 12 ){
-						$('#gradeEnd').text(character[1]).css({'background':'yellow'});
+					if (gradeNum >= 9 && gradeNum <= 12) {
+						$('#gradeEnd').text(character[1]).css({
+							'background': 'yellow'
+						});
 						gradeMethods(characterDetails[1]);
 					}
-					if(gradeNum >= 13 && gradeNum <= 16 ){
-						$('#gradeEnd').text(character[2]).css({'background':'blue'});
+					if (gradeNum >= 13 && gradeNum <= 16) {
+						$('#gradeEnd').text(character[2]).css({
+							'background': 'blue'
+						});
 						gradeMethods(characterDetails[2]);
 					}
-					if(gradeNum >= 17 && gradeNum <= 20 ){
-						$('#gradeEnd').text(character[3]).css({'background':'green'});
+					if (gradeNum >= 17 && gradeNum <= 20) {
+						$('#gradeEnd').text(character[3]).css({
+							'background': 'green'
+						});
 						gradeMethods(characterDetails[3]);
 					}
 
-
-
+					//显示标签
+					labelMethods(topicNum);
 				}
 			},
 		}
 	});
-
-	//第三题多选的处理（交叉排列组合）
-	var topicThreeArray = [
-		{
-			type:1,
-			text:'颜值'
-		},
-		{
-			type:2,
-			text:'轻薄'
-		},
-		{
-			type:3,
-			text:'游戏性能'
-		},
-		{
-			type:4,
-			text:'商务性能'
-		},
-		{
-			type:5,
-			text:'续行旅行'
-		},
-		{
-			type:6,
-			text:'储存性能'
-		},
-		{
-			type:7,
-			text:'品牌'
-		},
-	];
-	var data = ['a','b','c','d','e','f','g'];
- 
-	var getGroup = function(data, index = 0, group = []) {
-		var need_apply = new Array();
-		need_apply.push(data[index]);
-		for(var i = 0; i < group.length; i++) {
-			need_apply.push(group[i] + data[index]);
-		}
-		group.push.apply(group, need_apply);
-	 
-		if(index + 1 >= data.length) return group;
-		else return getGroup(data, index + 1, group);
-	}
-	 
-	console.log(getGroup(data));
-
-
-
-	var topicNum = [];//第三题选中个数
-	$('.topic-two').click(function(){
-		var type = $(this).attr('data-type');
-		var has_bgk = $(this).hasClass('btn-bgk-active');
-		if(has_bgk){
-			topicNum.push(type);
-			if(topicNum.length >= 2){//大于等于2的时候跳转下一题
-				mySwiper.slideTo(4); 
-			}
-		}else{
-			for(var i=0; i<topicNum.length; i++) {
-				if (topicNum[i] == type) {
-					topicNum.splice(i, 1);
-				}
-			}
-		}
-	})
-
-	//显示性格对应的8点
-	var gradeMethods = function(data){
-		var arr = [];
-		var item = data.text;
-		for (var j= 0; j < item.length; j++) {
-			arr.push(item[j])
-		}
-		var html = '';
-		for (var i = 0; i < arr.length; i++) {
-			html += '<div class="col-6" style="line-height:20px;font-size:0.6rem;">'+ arr[i] +'</div>';
-		}
-		$('#gradeMsg').html(html);
-	}
-
-
 	
+	//音乐
+	
+	var audio = document.getElementById('audio');
+	audio.play();
+	$(document).one("touchstart",
+		function() {
+			audio.play()
+		})
+		
+	$('.ceshiqiehuan').on('touchstart', function () {
+		audio.pause();
+		var luyinpath = '../audio/从古墓答题开始的音乐（用前30s）.mp3';
+		$('#audio').attr('src', luyinpath);
+		audio.play();
+	})
+});
 
 
-	// 
-	// 	$('body').on('touchstart',function(){ 
-	// 		audio.play();
-	// 	}); 
-
-
-
-})
+//音乐
