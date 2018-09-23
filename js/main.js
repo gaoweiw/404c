634f60f3099cee5d90f352e0d87785880924639c
gaowi <1104943970@qq.com>
2018-09-23 22:57:13 +0800

    干干干

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ $(function() {
 		$explain.addClass('animated bounceIn').css('display', 'block');
 	})
 
+
+
 	$('.btn-close').click(function() {
 		$explain.removeClass('bounceIn');
 		$explain.addClass('animated bounceOut');
@@ -81,7 +83,7 @@ $(function() {
 			topicNum.push(type);
 			if (topicNum.length == 2) { //等于2的时候判断结果页该展示的机型
 				$.ajax({
-					url: './js/data.json',
+					url: './json/data.json',
 					type: 'get',
 					datatype: 'json',
 					success: function(data) {
@@ -111,16 +113,10 @@ $(function() {
 		}
 	})
 
-	//性格标签查看详情页
-	$('#watchDetails').on('touchend', function() {
-		mySwiper.slideTo(12);
-	});
 	//结果页
-	$('#goBack').on('touchend', function() {
-		mySwiper.slideTo(10);
+	$('#goBack,#goBack2').on('touchend', function() {
+		mySwiper.slideTo(9);
 	});
-
-
 
 	//第三题多选的处理（交叉排列组合）
 
@@ -171,7 +167,7 @@ $(function() {
 		}
 		var html = '';
 		for (var i = 0; i < item.length; i++) { 
-			html += '<div class="col-6" style="line-height:2rem;font-size:0.8rem;">' + arr[i] + '</div>';
+			html += '<div class="col-6" style="line-height:1.5rem;font-size:0.8rem;">' + arr[i] + '</div>';
 		}
 		$('#gradeMsg').html(html);
 	}
@@ -180,11 +176,11 @@ $(function() {
 
 	var mySwiper = new Swiper('.swiper-container', {
 		// direction: 'vertical', //滑动方向
-		// initialSlide: 11,
+		// initialSlide: 9,
 		allowTouchMove: 0, //是否可以左右滑动
 		speed: 100, //切换速度
 
-		effect: 'cube', //切换效果
+		effect: 'fade', //切换效果
 
 		// 如果需要前进后退按钮
 		navigation: {
@@ -194,7 +190,7 @@ $(function() {
 		on: {
 			init: function() {
 				swiperAnimateCache(this); //隐藏动画元素 
-				swiperAnimate(this); //初始化完成开始动画
+				swiperAnimateCache(this); //初始化完成开始动画
 			},
 			slideChangeTransitionEnd: function() {
 				swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
@@ -243,13 +239,116 @@ $(function() {
 			audio.play()
 		})
 		
-	$('.ceshiqiehuan').on('touchstart', function () {
+	$('.ceshiqiehuan').on('touchend', function () {
 		audio.pause();
-		var luyinpath = '../audio/从古墓答题开始的音乐（用前30s）.mp3';
+		var luyinpath = './audio/music2.mp3';
 		$('#audio').attr('src', luyinpath);
 		audio.play();
 	})
+
+
+	//抽奖
+	//-------------------------------------------------------------------------------------------------------
+	var turnplate={
+		startAngle:0,			//开始角度
+		randomRate:[],              	//控制获奖率，百分制(相加需等于100%)，对应restaraunts(顺序需要保持一致)，
+		bRotate:false			//false:停止;ture:旋转
+	};
+	var sum;
+	turnplate.randomRate = ["0%", '0%', '0%', '0%', '0%', '0%'];
+	//设置turnplate.randomRate的数组长度就是大转盘格子的个数
+	var rotateTimeOut = function (){
+		$('.awardGift').rotate({
+			angle:0,
+			animateTo:2160,
+			duration:8000,
+			callback:function (){
+				alert('网络超时，请检查您的网络设置！');
+			}
+		});
+	};
+	//旋转
+	var rotateFn = function (item, txt){
+		var angles = item * (360 / turnplate.randomRate.length) - (360 / (turnplate.randomRate.length*2));
+		if(angles<270){
+			angles = 270 - angles; 
+		}else{
+			angles = 360 - angles + 270;
+		}
+		$('.awardGift').stopRotate();
+		$('.awardGift').rotate({
+			angle:0,
+			animateTo:angles+1800,
+			duration:8000,
+			callback:function (){ 
+				
+				//旋转完成调用的函数
+				// alert(txt.giftCode);
+				$('#showPrize').text(txt.giftCode);
+				$('.show-prize').css('display','block');
+				//开启旋转
+				turnplate.bRotate = !turnplate.bRotate;
+			}
+		});
+	};
+	//点击开始
+	$('.start').click(function (){
+		
+		if(turnplate.bRotate)return;
+		turnplate.bRotate = !turnplate.bRotate;
+
+		
+
+		$.get('json/award.json',function(data){
+			console.log(data);
+			turnplate.randomRate = ["0%", '0%', '0%', '0%', '0%', '0%'];
+			//从后台请求获取数据设置对应的数组为100%
+			
+			turnplate.randomRate[data.giftCode] = "100%";
+			//获取随机数(奖品个数范围内)
+			var item = rnd(turnplate.randomRate);
+			rotateFn(item,data);//data为你要带的参数
+		});
+		
+	});
+	//注意如果转盘对不准的情况下。可以调节awardGift下的img的旋转角度进去调节transform: rotate(22deg);。
+	$('.end-pack').on('touchstart',function(){
+		var attr = $(this).attr('data-id');
+		console.log(attr);
+		$('#' + attr).removeClass('hide').siblings().addClass('hide');
+		if(attr == 'boxCharacter'){//性格详情更换背景
+			$('#lastPage').removeClass('page-bgk').addClass('topic-12-bgk');
+		}else{
+			$('#lastPage').removeClass('topic-12-bgk').addClass('page-bgk');
+		}
+	})
+
+	
+	//-------------------------------------------------------------------------------------------------------
 });
-
-
-//音乐
+function rnd(rate){
+	var random = Math.floor(Math.random() * 100);
+	var myRandom = [];
+	var randomList = [];
+	var randomParent = [];
+	for(var i = 0; i < 100; i++){
+		myRandom.push(parseInt([i]) + 1);
+	}
+	for(var i = 0; i < rate.length; i++){
+		var temp = [];
+		var start = 0;
+		var end = 0;
+		randomList.push(parseInt(rate[i].split('%')[0]));
+		for(var j = 0; j < randomList.length; j++){
+			start += randomList[j-1] || 0
+			end += randomList[j]
+		}
+		temp = myRandom.slice(start, end);
+		randomParent.push(temp)
+	}
+	for(var i = 0; i < randomParent.length; i++){
+		if($.inArray(random, randomParent[i]) > 0){
+			return(i+1)
+		}
+	}
+}
